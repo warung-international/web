26a7fdac4a079243279555e52f8ea6ab10fbe469
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-05-01 20:39:54 +0700

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,13 @@ const Page = () => {
         <title>Warung International</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content="Warung International" />
+        <meta name="description" content="Warung International hanya menjual Soto Ayam terbaik di tata surya dan sudah teruji klinis di lab ITB dan IPB." />
+        <meta name="keywords" content="warung, international, warung international, aku ganteng" />
+        <meta name="robots" content="index, follow" />
+        <meta name="revisit-after" content="3 days" />
+        <meta name="author" content="mclemie" />
+        <meta property="og:image" content="https://cdn.discordapp.com/icons/922523614828433419/4f451776963ac9c0a8ed6abf5c2d16c4.png?size=4096" />
       </Head>
       <Link
         href="https://discord.gg/DnZ9GhQzCM"

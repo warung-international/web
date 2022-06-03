a3a1b250d5c7b24bb6f0e649a6f6e867d38eb1b1
Clemie McCartney <27568445+mclemie@users.noreply.github.com>
2022-06-03 08:17:28 +0700

    add simpleanalytics badge

--- discord-leaderboard/components/Footer.jsx
+++ discord-leaderboard/components/Footer.jsx
@@ -6,6 +6,8 @@ export const Footer = () => {
     <>
         <footer className="text-center text-muted footer-white">
         <br />
+        <a href="https://simpleanalytics.com/warunginternational.eu.org?utm_source=warunginternational.eu.org&utm_content=badge" referrerPolicy="origin" target="_blank" rel="noreferrer"><img src="https://simpleanalyticsbadges.com/warunginternational.eu.org?mode=dark" loading="lazy" crossOrigin="anonymous" /></a>
+        <br />
         Created with ❤️ by <Link href="https://mcley.me">mclemie</Link>
         <br /><br />
       </footer>

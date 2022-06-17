785c8e6ccda298cf8eeb02da648517ff952fc039
GNZTMPZ <27568445+mclemie@users.noreply.github.com>
2022-06-17 16:46:47 +0700

    new navbar view

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,44 +1,165 @@
 /*  ./components/Navbar.jsx     */
 import Link from 'next/link';
-import { Button, Drawer, Divider } from '@geist-ui/core'
-import { Menu, Home, Display, Book, Github } from '@geist-ui/icons'
-import React, { useState } from 'react'
+import React from 'react'
+import { Button } from "@geist-ui/core";
 
 export const Navbar = () => {
-    const [state, setState] = useState(false)
   return (
     <>
         <div className="nav">
-            <Button auto onClick={() => setState(true)}><Menu /></Button>
-            <Drawer visible={state} onClose={() => setState(false)} placement="right" mr="10px">
-                <Drawer.Subtitle>Main Menu</Drawer.Subtitle>
-                <Drawer.Content>
-                    <Link href="/">
-                        <a><Home /> Home</a>
-                    </Link> 
-                    <Divider />
-                    <Link href="/discord-leaderboard">
-                        <a><Display /> Leaderboards</a>
-                    </Link> 
-                    <Divider />
-                    <Link href="https://docs.warunginternational.eu.org">
-                        <a><Book /> Documentations</a>
-                    </Link>
-                    <Divider />
-                    <Link href="https://github.com/warung-international/web">
-                        <a><Github /> Github</a>
-                    </Link>
-                    <Divider />
-                    <iframe src="https://discord.com/widget?id=922523614828433419&theme=dark" width="300" height="500" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                </Drawer.Content>
-            </Drawer>
-            
+        <Link href="/">
+            <a className="nav__logo__wrapper">
+            <img
+                className='nav__logo'
+                src="../wi-logo.png"
+                alt="WI Logo"
+                ></img>
+            </a>
+        </Link>
+        <div className="nav__lhs">
+
+        <a 
+            className="nav_link black-link"
+            target="_blank"
+            rel='noreferrer'
+            href="/"
+        >Home</a>
+
+        <Link href="/discord-leaderboard">
+            <a className="nav_link black-link">Discord Leaderboard</a>
+        </Link>
+
+        <a 
+            className="nav_link black-link" 
+            target="_blank"
+            rel='noreferrer'
+            href="https://docs.warunginternational.eu.org/"
+        >Documentations</a>
+        <Link
+            className="nav_link black-link"
+            href="https://github.com/warung-international/web"
+        ><a className="nav_link black-link">Github</a></Link>
         </div>
-        <img
-          id="title"
-          src="../wi-logo.png"
-          alt="WI Logo"
-        ></img>
+        <div className="nav__rhs">
+            <Link href="https://discord.gg/">
+                <Button auto type="success">Discord</Button>
+            </Link>    
+        </div>
+        </div>
+        <style jsx>{`
+                .nav {
+                    flex-wrap: wrap;
+                    display: flex;
+                    align-items: center;
+                    background: var(--vercel-gray);
+                    height: 86px;
+                    overflow: hidden;
+                    padding-right: 5vw;
+                }
+
+                .nav__logo__wrapper {
+                    width: 9.4rem;
+                    position: absolute;
+                    height: 52px;
+                    top: 10px;
+                    left: 5vw;
+                }
+
+                .nav__logo {
+                    width: 50%;
+                }
+
+                .nav__lhs {
+                    flex-grow: 1;
+                    display: flex;
+                    align-items: center;
+                    padding-left: /*2vw;*/ 14.4rem;
+                    flex-wrap: wrap;
+                }
+
+                a.nav_link {
+                    cursor: pointer;
+                    margin-left: 5vw;
+                    cursor: pointer;
+                    font-weight: 500;
+                    transition: 0.15s ease;
+                    font-size: 1rem;
+                }
+                
+                
+                .nav_link:hover {
+                    color: var(--re-gray);
+                }
+
+
+                @media screen and (max-width: 812px) {
+                    .nav {
+                        height: auto;
+                    }
+
+                    .nav__lhs {
+                        padding-left: 20px;
+                        padding-top: 20px;
+                    }
+                    .nav__link__home {
+                        display: none;
+                    }
+                    a.nav_link {
+                        font-size: .8rem;
+                        margin-bottom: 10px;
+                        margin-left: 0;
+                        margin-right: 40px;
+                    }
+
+                    .nav__logo__wrapper {
+
+                        position: relative;
+                        width: 100%;
+                        flex-grow: 1;
+                        left: 0;
+                        padding-left: 15px;
+                    }
+
+                    .nav__logo {
+                        width: 3rem;
+                    }
+
+                    
+                }
+                
+                @media screen and (max-width: 768px) {
+
+                }
+                
+                @media screen and (max-width: 425px) {
+                    .nav {
+                        /*height: 50px;*/
+                        border-bottom: none;
+                    }
+                    .nav__logo__wrapper {
+                        height: auto;
+                        top: 5px;
+                    }
+
+                    .nav__logo {
+                        width: 5rem;
+                    }
+
+                    a.nav_link  {
+                        margin-right: 20px;
+                    }
+
+                    .nav-email {
+                        display: none;
+                    }
+                }
+                
+                /*
+                @media screen and (max-width: 320px) {
+                
+                }
+                */
+            `}</style>
     </>
   );
 };

ba0866b9a6ad324122573ae8089e9fd673321a3d
Clemie McCartney <27568445+clemiee@users.noreply.github.com>
2022-07-29 06:25:22 +0700

    sync _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import "../assets/css/main.css";
+import "../assets/css/scrolling-nav.css";
+import "../assets/css/animate.min.css";
+import "../assets/css/radial.css";
+import "../assets/css/style.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     <>

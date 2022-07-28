2f2830dc3561043a0f331ef1b04ecc735bb9b207
Clemie McCartney <27568445+clemiee@users.noreply.github.com>
2022-07-29 03:55:05 +0700

    ease up _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,9 @@
-import { GeistProvider, CssBaseline } from "@geist-ui/core";
-import React, { useState } from "react";
-import "inter-ui/inter.css";
-import "../styles/style.css";
-import "../styles/animate.min.css";
-
-const App = ({ Component, pageProps }) => {
-  const [themeType, setThemeType] = useState("dark");
-  const switchThemes = () => {
-    setThemeType((last) => (last === "dark" ? "light" : "dark"));
-  };
+function MyApp({ Component, pageProps }) {
   return (
-    <GeistProvider themeType={themeType}>
-      <CssBaseline />
+    <>
       <Component {...pageProps} />
-    </GeistProvider>
+    </>
   );
-};
+}
 
-export default App;
+export default MyApp;

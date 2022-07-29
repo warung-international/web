92868ce258f607260b80bba7a25d10493e92779e
Clemie McCartney <27568445+clemiee@users.noreply.github.com>
2022-07-29 07:38:21 +0700

    better roles

--- assets/components/Roles.jsx
+++ assets/components/Roles.jsx
@@ -17,16 +17,44 @@ export const Roles = () => {
               <div className="leaderboardSidebarBlockSeparator"></div>
               <div className="leaderboardSidebarBlockContent">
                 <div className="flex flexColumn bottom10">
-                  <h5>LEVEL 0</h5>
+                  <h5>LEVEL 5</h5>
                   <div className="flex flexRow flexWrap">
-                    <div className="leaderboardRole" style={{ borderColor: '#FFFFFF' }, { color: '#FFFFFF' }}>Joined
+                    <div className="leaderboardRole" style={{ borderColor: '#acf3de' }, { color: '#acf3de' }}>newcomer
                     </div>
                   </div>
                 </div>
                 <div className="flex flexColumn bottom10">
-                  <h5>LEVEL 1</h5>
+                  <h5>LEVEL 10</h5>
                   <div className="flex flexRow flexWrap">
-                    <div className="leaderboardRole" style={{ borderColor: '#FFFFFF' }, { color: '#FFFFFF' }}>Newbie
+                    <div className="leaderboardRole" style={{ borderColor: '#81f1d2' }, { color: '#81f1d2' }}>potential customer
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flexColumn bottom10">
+                  <h5>LEVEL 20</h5>
+                  <div className="flex flexRow flexWrap">
+                    <div className="leaderboardRole" style={{ borderColor: '#5ae4d9' }, { color: '#5ae4d9' }}>common customer
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flexColumn bottom10">
+                  <h5>LEVEL 50</h5>
+                  <div className="flex flexRow flexWrap">
+                    <div className="leaderboardRole" style={{ borderColor: '#4bd4d9' }, { color: '#4bd4d9' }}>well-known customer
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flexColumn bottom10">
+                  <h5>LEVEL 80</h5>
+                  <div className="flex flexRow flexWrap">
+                    <div className="leaderboardRole" style={{ borderColor: '#3dc3d9' }, { color: '#3dc3d9' }}>associates
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flexColumn bottom10">
+                  <h5>LEVEL 100</h5>
+                  <div className="flex flexRow flexWrap">
+                    <div className="leaderboardRole" style={{ borderColor: '#1cb2cd' }, { color: '#1cb2cd' }}>top commenters
                     </div>
                   </div>
                 </div>
